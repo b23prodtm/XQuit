28f502c8fde875fb88268fbda343097fdd1bba8e
Mac Bok Arimana <b23prodtm@users.sourceforge.net>
2018-05-06 12:34:47 +0200

    Continous Integration Build Products with macOS xcodebuild Upload releases with travis-ci

---
+++ DerivedData/Build/Intermediates/XQuit.build/Debug/XQuit.build/Objects-normal/x86_64/NSObject_Extension.d
@@ -0,0 +1,4 @@
+dependencies: \
+  /Users/wwwb23prodtminfo/Documents/GitHub/XQuit/XQuit/NSObject_Extension.m \
+  /Users/wwwb23prodtminfo/Documents/GitHub/XQuit/XQuit/NSObject_Extension.h \
+  /Users/wwwb23prodtminfo/Documents/GitHub/XQuit/XQuit/XQuit.h
---
+++ DerivedData/Build/Intermediates/XQuit.build/Debug/XQuit.build/Objects-normal/x86_64/XQuit.d
@@ -0,0 +1,3 @@
+dependencies: \
+  /Users/wwwb23prodtminfo/Documents/GitHub/XQuit/XQuit/XQuit.m \
+  /Users/wwwb23prodtminfo/Documents/GitHub/XQuit/XQuit/XQuit.h
